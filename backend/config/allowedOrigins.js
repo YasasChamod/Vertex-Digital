47c0123bfe7a0c82708fc02ffd8e2da308c3bf86
Yasas <yasaschamod1234@gmail.com>
2026-05-22 14:41:59 +0530

    Ready to deploy

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:5174',
+    'https://vertex-digital-alpha.vercel.app',
     'https://www.yasasrepairshop.com',
     'https://yasasrepairshop.com'
 ];
